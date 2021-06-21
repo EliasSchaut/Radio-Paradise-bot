849672738b3a9e5690dfcba95b1767e1cf177e4b
EliasSchaut <eschaut@outlook.de>
2021-06-21 12:58:51 +0200

    fill join.js

--- commands/channel/join.js
+++ commands/channel/join.js
@@ -1,17 +1,18 @@
 const { lang } = require("../../config/config.json");
+const connection_manager = require("../../js/connection_manager");
 const text = require(`../../config/text_${lang}.json`).commands.join;
 
 module.exports = {
     name: 'join',
     description: text.help,
     aliases: [''],
-    args: true,
-    usage: text.usage,
-    args_min_length: 1,
-    guildOnly: false,
+    args: false,
+    guildOnly: true,
     dmOnly: false,
     restricted: false,
     async execute(message, args) {
-
+        const voice_channel = await message.guild.channels.cache.get("801473859656089673")
+        const connection = await voice_channel.join();
+        connection_manager.connect(connection);
     },
 };
